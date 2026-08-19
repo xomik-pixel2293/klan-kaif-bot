9fe7ce74caaeb900ceba72babc82be8e7fabba1a
xomik-pixel2293 <xomik2293@gmail.com>
2026-08-20 01:34:40 +0500

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -301,7 +301,7 @@ def select_clan_for_role_buttons_simple(clans, role_type, user_id, username):
         emoji = emojis.get(clan_id, '🔵')
         buttons.append([InlineKeyboardButton(
             text=f"{emoji} {clan_name}",
-            callback_data=f"assign_to_clan_simple_{role_type}_{clan_id}_{user_id}_{username}"
+            callback_data=f"clan_assign_{role_type}_{clan_id}_{user_id}_{username}"
         )])
     buttons.append([InlineKeyboardButton(text='🔙 Назад', callback_data='back_to_admin')])
     return InlineKeyboardMarkup(inline_keyboard=buttons)
